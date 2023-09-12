22c3acb88dbf116f4517eccaadea7c0fe3dd3282
Ángel Castañeda <angelaxel1420@disroot.org>
2023-09-12 16:43:08 -0500

    Fix 1.03 sos-top-two
    My sorting system was not enough.

--- ch1/1.03.scm
+++ ch1/1.03.scm
@@ -16,7 +16,10 @@
     ((and (> x y) (<= y z)) (sos x z))
     (else (sos y z))))
 
+"These should all be the same"
 (sos-top-two 1 2 3)
+(sos-top-two 1 3 2)
 (sos-top-two 2 1 3)
+(sos-top-two 2 3 1)
+(sos-top-two 3 1 2)
 (sos-top-two 3 2 1)
-(sos-top-two 2 4 4)
