060cb8f04b0e0eff6f7114a6685103a5f028dfc1
Ángel Castañeda <angelaxel1420@disroot.org>
2023-09-13 01:09:50 -0500

    Clarify 1.06
    Made some of the language clearer for what turning if into a procedure
    does.

--- ch1/1.06.scm
+++ ch1/1.06.scm
@@ -8,8 +8,13 @@
 (my-if (= 1 1) 0 5)
 
 ;; works great with atoms like 0 and 5, but exercise 1.05 showed that as long
-;; as you use applicative-order eval, any recursive definition like
-;; good-enough? is gonna get messed up in a procedure. the real if is a special
-;; form to not have to deal with this kind of problem.
+;; as you use applicative-order eval on a procedure, any recursive definition like
+;; good-enough? could get hung up on itself like (p) in the previous exercise. 
+;; the real if is a special form to not have to deal with this kind of problem.
+
+;; another way of rewording this is that you basically made a procedure funnel
+;; for a special order expression, but the funnel is less a funnel and more of
+;; an asthmatic throat that likes to close up whenever its applicative-order 
+;; condition acts up.
 
 ;; what we really need to learn is how to make our own special forms...
